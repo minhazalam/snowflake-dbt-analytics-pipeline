aba1a12defa6d7b69e3a3b482ae762307a827132
Minhaz Alam <minhazalam365@gmail.com>
2026-01-06 01:38:09 +0530

    add generic test level warning

--- tests/generic/minimum_row_count.sql
+++ tests/generic/minimum_row_count.sql
@@ -1,3 +1,4 @@
 {% test minimum_row_count(model, min_rows) %}
+{{ config(severity='warn') }} -- Set default severity to 'warn'
 select count(*) as cnt_row from {{ model }} having count(*) < {{ min_rows }}
 {% endtest %}
