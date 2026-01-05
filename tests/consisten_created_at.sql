0edfe4d2515ebfea3db3d3462d2fcff2003eeb53
Minhaz Alam <minhazalam365@gmail.com>
2026-01-05 15:54:04 +0530

    add singular test-listing date is greater than equals review date

---
+++ tests/consisten_created_at.sql
@@ -0,0 +1,5 @@
+-- Fails if a review exists before the listing was created
+select * from {{ ref('dim_listings_cleansed') }} l
+inner join {{ ref('fct_reviews')}} r
+using (listing_id)
+where l.created_at >= r.review_date
